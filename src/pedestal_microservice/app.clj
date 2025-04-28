b616dae64c0084f707ab22e8e8b63cea9f2f1698
Marcos Martinez <marcos@deb-t14-pf1am4uh.marcosmartinez.net>
2025-04-27 20:37:09 -0700

    Renamed file

--- src/pedestal_microservice/app.clj
+++ src/pedestal_microservice/app.clj
@@ -1,13 +1,13 @@
-(ns pedestal-microservice.core
+(ns pedestal-microservice.app
   (:require [com.stuartsierra.component :as component]
             [io.pedestal.http :as http]
+            [pedestal-microservice.echo.core :as echo]
             [pedestal-microservice.service.core :as service]
             [pedestal-microservice.server.components.server :as server-component]
             [pedestal-microservice.service.components.service :as service-component]
-            [pedestal-microservice.service.components.routes :as routes-component]
-            [pedestal-microservice.echo.core :as echo]))
+            [pedestal-microservice.service.components.routes :as routes-component]))
 
-(def RoutesComponent
+(defn build-routes []
   (routes-component/new-routes
    echo/routes
    service/routes))
@@ -15,7 +15,7 @@
 (defn -main []
   (println "Starting application...")
   (-> (component/system-map
-       :routes RoutesComponent
+       :routes (build-routes)
        :service (service-component/new)
        :server (server-component/new))
-      (component/start-system)))
+      (component/start)))
