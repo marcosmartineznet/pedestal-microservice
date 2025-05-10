d996c5f18879e6f651488c96a55a60c359b0ac84
Marcos Martinez <marcos@deb-t14-pf1am4uh.marcosmartinez.net>
2025-05-09 21:24:41 -0700

    Add get listing by id handler

--- src/pedestal_microservice/service/listing/routes/core.clj
+++ src/pedestal_microservice/service/listing/routes/core.clj
@@ -8,13 +8,19 @@
         listing (controller/create-listing! ListingController input)]
     {:status 200 :body listing}))
 
-(defn get-all-listings-handler [request]
-  (let [{:keys [dependencies]} request
-        ListingController (:ListingController dependencies)
+(defn get-listing-by-id-handler [{:keys [dependencies] :as request}]
+  (let [ListingController (:ListingController dependencies)
+        id (-> request :path-params :id Long/parseLong)
+        listing (controller/get-by-id ListingController id)]
+    {:status 200 :body listing}))
+
+(defn get-all-listings-handler [{:keys [dependencies] :as request}]
+  (let [ListingController (:ListingController dependencies)
         result (controller/get-all ListingController)]
     {:status 200 :body {:data result}}))
 
 
 (def specs
   #{["/listing" :get `get-all-listings-handler]
-    ["/listing" :post [(body-params) `create-listing-handler]]})
+    ["/listing" :post [(body-params) `create-listing-handler]]
+    ["/listing/:id" :get [(body-params) `get-listing-by-id-handler]]})
